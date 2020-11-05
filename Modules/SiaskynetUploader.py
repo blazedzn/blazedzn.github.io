6588a1960b5760fbf61b1344bac1639095ce44c8
blazedzn <54804805+blazedzn@users.noreply.github.com>
2020-11-05 13:55:42 +0300

    fdgds

--- Modules/SiaskynetUploader.py
+++ Modules/SiaskynetUploader.py
@@ -1,5 +1,6 @@
 from .. import loader, utils
 import siaskynet as skynet
+import os
 
 @loader.tds
 class IPLookupMod(loader.Module):
@@ -14,8 +15,18 @@ class IPLookupMod(loader.Module):
         if not reply:
             await message.edit("Нужен реплай")
             return
-        file = await reply.download_media()
-        link = client.upload_file(file)
-        filtered = link.split('sia://')
-        link = 'https://siasky.net/' +  str(filtered[1])
-        await message.edit("Линк: \n" + link)
+        try:
+            file = await reply.download_media()
+            link = client.upload_file(file)
+            filtered = link.split('sia://')
+            link = 'https://siasky.net/' +  str(filtered[1])
+            await message.edit("Линк: \n" + link)
+        except:
+            f = open('text.txt', 'w')
+            f.write(reply.raw_text)
+            f.close()
+            link = client.upload_file("text.txt")
+            filtered = link.split('sia://')
+            link = 'https://siasky.net/' +  str(filtered[1])
+            await message.edit("Линк: \n" + link)
+            os.remove('text.txt')
